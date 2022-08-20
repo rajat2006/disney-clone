0550c520d2f42c4e4a73c91fb5ac4c9b30e0f285
Rajat Gedam <rajatgedam@Rajats-MacBook-Air.local>
2022-08-20 11:27:32 +0530

    add login component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello from Disney Clone</h2>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
